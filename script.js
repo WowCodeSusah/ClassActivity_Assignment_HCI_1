592f4cdcefa4e43ae418a73e5ef10c35c864d5ff
WowCodeSusah <114371708+WowCodeSusah@users.noreply.github.com>
2022-12-21 08:48:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //Please put your answer here
+$('#signup-modal').hide()
+$('.signup-button').click(function(){
+    $('#signup-modal').fadeIn("slow")
+    });
+$('#close-button').click(function(){
+    $('#signup-modal').fadeOut("slow")
+});
